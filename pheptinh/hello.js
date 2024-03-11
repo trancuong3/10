8eed52bf09a73ae672979ae6b61b4598772b4be6
trancuong3 <161544695+trancuong3@users.noreply.github.com>
2024-03-11 21:15:13 +0700

    pheptinh

---
+++ pheptinh/hello.js
@@ -0,0 +1,30 @@
+
+function cong(){
+ let a= document.getElementById("a").value;
+ let b= document.getElementById("b").value;
+ let x=a+b;
+ document.getElementById("result").innerHTML =x;
+
+}
+function tru(){
+    let a= document.getElementById("a").value;
+    let b= document.getElementById("b").value;
+    let x=a-b;
+    document.getElementById("result").innerHTML =x;
+   
+   }
+   function nhan(){
+    let a= document.getElementById("a").value;
+    let b= document.getElementById("b").value;
+    let x=a*b;
+    document.getElementById("result").innerHTML =x;
+   
+   }
+   function chia(){
+    let a= document.getElementById("a").value;
+    let b= document.getElementById("b").value;
+    let x=a/b;
+    document.getElementById("result").innerHTML =x;
+   
+   }
+
